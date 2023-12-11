845b755fb777d6268f128d7026f803ce8bd7ac2c
murasaki_violet <tobiratory.murasaki@gmail.com>
2023-12-11 21:12:10 +0900

    リファクタ

--- src/App.js
+++ src/App.js
@@ -25,29 +25,6 @@ const Board = ({ xIsNext, squares, onPlay }) => {
     onPlay(nextSquares);
   }
 
-  
-
-
-  const calculateWinner = (squares) => {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
-
   const winner = calculateWinner(squares);
   let status;
   if (winner) {
@@ -96,15 +73,16 @@ const Game = () =>{
 
   
 
-  const moves = history.map((move) => {
+  const moves = history.map((squares, move) => {
     let description;
+    console.log(squares, move)
     if (move > 0) {
       description = 'Go to move #' + move;
     } else {
       description = 'Go to game start';
     }
     return (
-      <li key = {move}>
+      <li key={move}>
         <button onClick={() => jumpTo(move)}>{description}</button>
       </li>
     );
@@ -122,6 +100,26 @@ const Game = () =>{
   );
 } 
 
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+
 export default Game;
 
-//export default Board;
